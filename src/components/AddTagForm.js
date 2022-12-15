be30369bc6a5e1d88794240614dcc52cb6ab22c8
micpob <bomboloapp@gmail.com>
2022-12-16 00:05:49 +0100

    Add new tag validation

--- src/components/AddTagForm.js
+++ src/components/AddTagForm.js
@@ -1,6 +1,6 @@
 import React, { Component, useState, useRef, useEffect } from 'react'
 import styled from 'styled-components/native'
-import { SafeAreaView, StyleSheet, TextInput, Text, View, Platform, Button, TouchableOpacity  } from 'react-native';
+import { SafeAreaView, StyleSheet, TextInput, Text, View, Platform, Button, TouchableOpacity, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
@@ -35,16 +35,54 @@ const AddTagForm = ({ site, allTags, setAllTags }) => {
   const [newTag, onChangeText] = React.useState('')
 
   const handleSubmit = (value) => {
-    const tagToAdd = {name: value, selected: false}
-    console.log('tagToAdd:', tagToAdd)
-    console.log('allTags:', allTags)
 
-    const newAllTags = [...allTags, tagToAdd]
-    console.log('newAllTags:', newAllTags)
-    const jsonNewTagsArray = JSON.stringify(newAllTags)
-    AsyncStorage.setItem(`${site}-tags`, jsonNewTagsArray)
-    setAllTags(newAllTags)
-    onChangeText('')
+    if (value.length < 1) return
+
+    const tagAlreadyExists = allTags.some(tag => {
+      return tag.name.toLowerCase() === value.toLowerCase()
+    })
+    console.log('tag already exists:', tagAlreadyExists)
+    if (tagAlreadyExists) {
+      Alert.alert(
+        "Tag already exists",
+        `the tag "${value}" is already added`,
+        [ ],
+        {
+          cancelable: true
+        }
+      )
+    } else {
+      const url = `https://api.stackexchange.com/2.3/search/advanced?pagesize=5&order=desc&sort=activity&tagged=${value}&site=${site}&filter=!0ynczPwaq3R_qM75`
+
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log('data:', data)
+        if (typeof data.items == 'undefined') { console.log('data.items undefined'); setPage(1); return}
+        if (data.items.length === 0) {
+          Alert.alert(
+            "Tag does not exists",
+            `The tag "${value}" does not exist on ${site}. 
+
+Please try with another tag.`,
+            [ ],
+            {
+              cancelable: true
+            }
+          )
+        } else {
+          const tagToAdd = {name: value, selected: false}
+          console.log('tagToAdd:', tagToAdd)
+          console.log('allTags:', allTags)
+          const newAllTags = [...allTags, tagToAdd]
+          console.log('newAllTags:', newAllTags)
+          const jsonNewTagsArray = JSON.stringify(newAllTags)
+          AsyncStorage.setItem(`${site}-tags`, jsonNewTagsArray)
+          setAllTags(newAllTags)
+          onChangeText('')
+        }
+      })
+    }
   }
   
   return (
