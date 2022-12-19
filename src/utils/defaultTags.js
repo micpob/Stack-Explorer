3e0db693ed41f35edfe021e77d591a808b6bf80c
micpob <bomboloapp@gmail.com>
2022-12-19 12:57:06 +0100

    Add function to get most popular tags

--- src/utils/defaultTags.js
+++ src/utils/defaultTags.js
@@ -1,5 +1,5 @@
 
-const stackoverflow = [
+/* const stackoverflow = [
     {name: 'HTML', selected: true}, 
     {name: 'CSS', selected: true}, 
     {name: 'JavaScript', selected: true}, 
@@ -8,6 +8,39 @@ const stackoverflow = [
     {name: 'mongo', selected: false}, 
     {name: 'PHP', selected: false}, 
     {name: 'Node.js', selected: false}, 
-  ] 
+  ]  */
+
+const stackoverflow = [
+  { name: '.net', selected: false },
+  { name: 'android', selected: false },
+  { name: 'angular', selected: false },
+  { name: 'arrays', selected: false },
+  { name: 'asp.net', selected: false },
+  { name: 'c', selected: false },
+  { name: 'c#', selected: false },
+  { name: 'c++', selected: false },
+  { name: 'css', selected: false },
+  { name: 'django', selected: false },
+  { name: 'excel', selected: false },
+  { name: 'html', selected: false },
+  { name: 'ios', selected: false },
+  { name: 'java', selected: true },
+  { name: 'javascript', selected: true },
+  { name: 'jquery', selected: false },
+  { name: 'json', selected: false },
+  { name: 'mysql', selected: false },
+  { name: 'node.js', selected: false },
+  { name: 'objective-c', selected: false },
+  { name: 'pandas', selected: false },
+  { name: 'php', selected: false },
+  { name: 'python', selected: true },
+  { name: 'python-3.x', selected: false },
+  { name: 'r', selected: false },
+  { name: 'reactjs', selected: false },
+  { name: 'ruby-on-rails', selected: false },
+  { name: 'sql', selected: false },
+  { name: 'sql-server', selected: false },
+  { name: 'swift', selected: false }
+]
 
 export default {stackoverflow}
---
+++ src/utils/getMostPopularTags.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+
+const url = `https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow`
+
+fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        //console.log('data:', data.items)
+        const tagsArray = data.items.map((obj, index) => {
+          const tagObject = {
+            name: obj.name,
+            selected: index < 3 ? true : false
+          }
+          return tagObject
+        })
+        tagsArray.sort((a, b) => a.name.localeCompare(b.name))
+        console.log('tagsArray:', tagsArray)
+      })
