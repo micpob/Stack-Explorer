ae9e4da589235e0be3e8eeb864bca95a34db4fad
micpob <bomboloapp@gmail.com>
2022-12-21 00:14:56 +0100

    Make button bigger

--- src/components/TagButton.js
+++ src/components/TagButton.js
@@ -9,6 +9,7 @@ const MainContainer = styled.View`
   border-radius: 8px;
   padding: 4px;
   margin: 4px;
+  min-width: 32px;
   background: ${props => props.selected ? 'black' : 'white'}
 `
 
@@ -20,8 +21,9 @@ const StyledTextButton = styled.Text`
   color:  ${props => props.selected ? 'white' : 'black'}
   font-size: 16px;
   font-weight: 600;
+  text-align: center;
 `
-const TagButton = ({title, selected, handleClick}) => {
+const TagButton = ({title, selected, handleClick, handleLongPress}) => {
 
   /* const [isSelected, setIsSelected] = useState(selected)
 
@@ -29,10 +31,12 @@ const TagButton = ({title, selected, handleClick}) => {
 
     setIsSelected(!isSelected)
   } */
+
+
   
   return (
     <MainContainer selected={selected}>
-      <StyledTagButton onPress={handleClick} >
+      <StyledTagButton onPress={handleClick} onLongPress={handleLongPress} >
         <StyledTextButton selected={selected}>{title}</StyledTextButton>
       </StyledTagButton>
     </MainContainer>
