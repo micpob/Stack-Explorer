7b6e230652a392714984e02a0199992dcaa3dc98
micpob <bomboloapp@gmail.com>
2022-12-26 07:47:24 +0100

    Add icon

--- src/components/ShowSettingsButton.js
+++ src/components/ShowSettingsButton.js
@@ -2,8 +2,10 @@ import React, { Component, useState, useRef, useEffect } from 'react'
 import styled from 'styled-components/native'
 import { StyleSheet, Text, View, Platform, SafeAreaView, Button, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ionicons } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons';
 
-const MainContainer = styled.View`
+/* const MainContainer = styled.View`
   display: flex;
   align-items: center;
   align-self: center;
@@ -11,17 +13,23 @@ const MainContainer = styled.View`
   border: 2px solid black;
   border-radius: 6px;
   padding: 7px;
+` */
+const MainContainer = styled.View`
+  display: flex;
+  align-items: center;
+  align-self: center;
+  width: 25%;
 `
 
-const StyledTagsButton = styled.TouchableOpacity`
+const StyledButton = styled.TouchableOpacity`
   
 `
 
-const StyledTextButton = styled.Text`
+/* const StyledTextButton = styled.Text`
   color: white;
   font-size: 25px;
   font-weight: 600;
-`
+` */
 
 const ShowSettingsButton = ({setShowAlert, setShowSettingsView}) => {
   
@@ -32,9 +40,13 @@ const ShowSettingsButton = ({setShowAlert, setShowSettingsView}) => {
   
   return (
     <MainContainer>
-      <StyledTagsButton onPress={handleClick}>
+      <StyledButton onPress={handleClick}>
+        <Ionicons name="settings-outline" size={36} color="gray" />
+        {/* <AntDesign name="setting" size={36} color="black" /> */}
+      </StyledButton>
+      {/* <StyledTagsButton onPress={handleClick}>
         <StyledTextButton>TAGS</StyledTextButton>
-      </StyledTagsButton>
+      </StyledTagsButton> */}
     </MainContainer>  
   )
 }
