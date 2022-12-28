0c48540d24fe8fd57bec474c433d7d727f3a9b48
micpob <bomboloapp@gmail.com>
2022-12-28 10:37:29 +0100

    Add new sites

---
+++ src/utils/defaultSites.js
@@ -0,0 +1,11 @@
+
+const sites = [
+  {value: 'askubuntu', name:'Ask Ubuntu'},
+  {value: 'italian', name:'Italian Language'},
+  {value: 'cooking', name:'Seasoned Advice'},
+  {value: 'serverfault', name:'Server Fault'},
+  {value: 'stackoverflow', name:'Stack Overflow'},
+  {value: 'superuser', name:'Superuser'},
+]
+
+export default {sites}
