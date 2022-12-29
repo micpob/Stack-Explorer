66f57242c29f7f84ed479c84903004b77f0e67be
micpob <bomboloapp@gmail.com>
2022-12-29 10:58:05 +0100

    Change select modal

--- src/components/SitePicker.js
+++ src/components/SitePicker.js
@@ -1,34 +1,57 @@
 import React, { Component, Fragment, useState, useRef, useEffect } from 'react'
 import styled from 'styled-components/native'
 import { StyleSheet, Text, View, Platform, SafeAreaView, Button, TouchableOpacity  } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import {Picker} from '@react-native-picker/picker'
+import ModalSelector from 'react-native-modal-selector'
+import colors from '../utils/colors'
 import defaultSites from '../utils/defaultSites'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import SearchableDropdown from 'react-native-searchable-dropdown'
 
 const MainContainer = styled.View`
   display: flex;
-  flex: 1;
-  margin-right: auto;
-  font-family: 'Montserrat-Medium';
+  margin: auto;
+  min-width: 60%;
+  max-width: 84%;
 `
-const SitePicker = ({site, setSite}) => {
+const SitePicker = ({site, setSite, getStoredtags, setTags}) => {
 
-  const onSelect = (value) => {
+  const onSelect = async (value) => {
     const jsonNewSelectedSite = JSON.stringify(value)
     AsyncStorage.setItem(`site`, jsonNewSelectedSite)
     setSite(value)
+    const storedTags = await getStoredtags(value)
+    const selectedTags = storedTags.filter(tagObject => tagObject.selected).map(selectedTagObject => selectedTagObject.name)
+    setTags(selectedTags)
   }
+
+  const data = defaultSites['sites'].map((siteObj, index) => {
+      return { key: index, label: siteObj.name, value: siteObj.value }
+    }
+  )
+
+  const getInitValue = () => {
+    const initValue = defaultSites['sites'].find(siteObj => siteObj.value === site)
+    return initValue.name
+  }
+
 
   return (
     <MainContainer>
-      <Picker
-        //mode='dropdown'
-        selectedValue={site}
-        onValueChange={(value, index) => { onSelect(value) }
-        }>
-        {defaultSites['sites'].map(site => <Picker.Item style={{fontFamily: 'Montserrat-Medium', fontSize: 24, fontWeight: 600}} key={site} label={site} value={site} />)}  
-      </Picker>
+      <ModalSelector
+        data={data}
+        initValue={getInitValue()}
+        onChange={(option)=> { onSelect(option.value) }}
+        initValueTextStyle={{color: 'black', fontSize: 20, fontWeight: '600'}}
+        overlayStyle={{flex: 1, padding: '5%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.7)'}}    
+        optionContainerStyle={{backgroundColor: 'white', minWidth: '80%', maxWidth: '90%', maxHeight: 600 }}  
+        optionTextStyle= {{color: colors.selectText, fontSize: 22, fontWeight: '600'}}
+        animationType='fade'
+        backdropPressToClose={true}
+        cancelContainerStyle={{display: 'none'}}
+        selectStyle={{backgroundColor: 'white', borderColor: 'black', paddingLeft: 12, paddingRight: 12 }}
+        /* selectedItemTextStyle={{color: 'black'}} */
+      />
     </MainContainer>
   )
 }
--- src/components/YearPicker.js
+++ src/components/YearPicker.js
@@ -4,39 +4,65 @@ import { StyleSheet, Text, View, Platform, SafeAreaView, Button, TouchableOpacit
 import {Picker} from '@react-native-picker/picker';
 import defaultYears from '../utils/defaultYears'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import ModalSelector from 'react-native-modal-selector'
+import colors from '../utils/colors'
 
 const MainContainer = styled.View`
   display: flex;
-  flex: 1;
-  margin-right: auto;
+  margin: auto;
+  min-width: 35%;
 `
-
 const StyledText = styled.Text`
   font-size: ${props => props.selected ? '32px' : '24px'};
   opacity: ${props => props.selected ? '1' : '0.5'};
   font-weight: 600;
-  text-align: center;
 `
 const YearPicker = ({year, setYear}) => {
 
   //console.log('years:', defaultYears)
 
-  const onSelect = (value) => {
-    const newSelectedYear = defaultYears['years'].find(year => year.value === value)
-    const jsonNewSelectedYear = JSON.stringify(newSelectedYear)
+  const onSelect = async (value) => {
+    console.log('onSlect')
+    const jsonNewSelectedYear = JSON.stringify(value)
+    console.log('jsonNewSelectedYear:', jsonNewSelectedYear)
     AsyncStorage.setItem(`year`, jsonNewSelectedYear)
-    setYear(newSelectedYear)
+    setYear(value)
+  }
+
+  console.log('defaultYears[]', defaultYears['years'])
+
+  const data = defaultYears['years'].map((yearObj, index) => {
+      return { key: index, label: yearObj.name, value: yearObj.value }
+    }
+  )
+
+  const getInitValue = () => {
+    const initValue = defaultYears['years'].find(yearObj => yearObj.value === year)
+    console.log('initvalue:', initValue)
+    return initValue.name
   }
 
   return (
     <MainContainer>
-      <Picker
-        //mode='dropdown'
-        selectedValue={year.value}
+      <ModalSelector
+        data={data}
+        initValue={getInitValue()}
+        onChange={(option)=> { onSelect(option.value) }}
+        initValueTextStyle={{color: 'black', fontSize: 20, fontWeight: '600'}}
+        overlayStyle={{flex: 1, padding: '5%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.7)'}}    
+        optionContainerStyle={{backgroundColor: 'white', minWidth: '80%', maxWidth: '90%', maxHeight: 600 }}  
+        optionTextStyle= {{color: colors.selectText, fontSize: 22, fontWeight: '600'}}
+        animationType='fade'
+        backdropPressToClose={true}
+        cancelContainerStyle={{display: 'none'}}
+        selectStyle={{backgroundColor: 'white', borderColor: 'black', paddingLeft: 12, paddingRight: 12 }}
+      />
+      {/* <Picker
+        selectedValue={year}
         onValueChange={(value, index) => { onSelect(value) }
         }>
-        {defaultYears['years'].map(yearObj => <Picker.Item style={{fontSize: 24, fontWeight: 600}} key={yearObj.label} label={yearObj.label} value={yearObj.value} />)}  
-      </Picker>
+        {defaultYears['years'].map(yearObj => <Picker.Item style={{fontSize: 22, color: 'black'}} key={yearObj.name} label={yearObj.name} value={yearObj.value} />)}  
+      </Picker> */}
     </MainContainer>
   )
 }
