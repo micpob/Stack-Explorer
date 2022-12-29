10f210e21840317d0c3d2395840723f349903c4b
micpob <bomboloapp@gmail.com>
2022-12-29 10:58:58 +0100

    Add new sites

--- src/utils/defaultSites.js
+++ src/utils/defaultSites.js
@@ -1,11 +1,15 @@
 
 const sites = [
-  {value: 'askubuntu', name:'Ask Ubuntu'},
-  {value: 'italian', name:'Italian Language'},
-  {value: 'cooking', name:'Seasoned Advice'},
-  {value: 'serverfault', name:'Server Fault'},
-  {value: 'stackoverflow', name:'Stack Overflow'},
-  {value: 'superuser', name:'Superuser'},
+  {value: 'askubuntu', name: 'Ask Ubuntu'},
+  {value: 'english', name: 'English Language & Usage'},
+  {value: 'italian', name: 'Italian Language'},
+  {value: 'math', name: 'Mathematics'},
+  {value: 'scifi', name: 'Science Fiction & Fantasy'},
+  {value: 'cooking', name: 'Seasoned Advice'},
+  {value: 'serverfault', name: 'Server Fault'},
+  {value: 'stackoverflow', name: 'Stack Overflow'},
+  {value: 'superuser', name: 'Superuser'},
+  {value: 'travel', name: 'Travel'},
 ]
 
 export default {sites}
