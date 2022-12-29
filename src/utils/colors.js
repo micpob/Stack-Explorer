f8dcf6362e1b902cbcfd6412e1276620985a31d5
micpob <bomboloapp@gmail.com>
2022-12-30 00:03:24 +0100

    Add standard colors

---
+++ src/utils/colors.js
@@ -0,0 +1,19 @@
+export default colors = {
+  primary: '#408080', 
+  buttonsBar: '#764C90',
+  buttonsBarIcons: 'lightgray',
+  deleteMain: 'orange',
+  settingsBackground: '#F4F4F4',
+  settingsBoxBackground: '#c1e1ec',
+  settingsTitles: '#408080',
+  selectText: '#408080'
+}
+/* export default colors = {
+  buttonsBar: '#764C90',
+  buttonsBarIcons: 'lightgray',
+  deleteMain: 'orange',
+  settingsBackground: '#F4F4F4',
+  settingsBoxBackground: '#ADD8E6',
+  settingsTitles: '#408080',
+  selectText: '#408080'
+} */
