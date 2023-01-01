5d2ae55f0d2acdc7b428344f7f0926844e768823
micpob <bomboloapp@gmail.com>
2023-01-01 13:32:09 +0100

    Add search bar in site picker

--- src/components/SitePickerSearchable.js
+++ src/components/SitePickerSearchable.js
@@ -1,12 +1,10 @@
-import React, { Component, Fragment, useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styled from 'styled-components/native'
-import { StyleSheet, Text, View, Platform, SafeAreaView, Button, TouchableOpacity  } from 'react-native';
-import {Picker} from '@react-native-picker/picker'
-import ModalSelector from 'react-native-modal-selector'
+import { Text, View, TouchableOpacity, TextInput  } from 'react-native';
+import ModalSelector from 'react-native-modal-selector-searchable'
 import colors from '../utils/colors'
 import defaultSites from '../utils/defaultSites'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import SearchableDropdown from 'react-native-searchable-dropdown'
 
 const MainContainer = styled.View`
   display: flex;
@@ -14,13 +12,14 @@ const MainContainer = styled.View`
   min-width: 60%;
   max-width: 84%;
 `
-const SitePicker = ({site, setSite, getStoredtags, setTags}) => {
+
+const SitePickerSearchable = ({site, setSite, getStoredTags, setTags}) => {
 
   const onSelect = async (value) => {
     const jsonNewSelectedSite = JSON.stringify(value)
     AsyncStorage.setItem(`site`, jsonNewSelectedSite)
     setSite(value)
-    const storedTags = await getStoredtags(value)
+    const storedTags = await getStoredTags(value)
     const selectedTags = storedTags.filter(tagObject => tagObject.selected).map(selectedTagObject => selectedTagObject.name)
     setTags(selectedTags)
   }
@@ -35,7 +34,6 @@ const SitePicker = ({site, setSite, getStoredtags, setTags}) => {
     return initValue.name
   }
 
-
   return (
     <MainContainer>
       <ModalSelector
@@ -47,13 +45,11 @@ const SitePicker = ({site, setSite, getStoredtags, setTags}) => {
         optionContainerStyle={{backgroundColor: 'white', minWidth: '80%', maxWidth: '90%', maxHeight: 600 }}  
         optionTextStyle= {{color: colors.primary, fontSize: 22, fontWeight: '600'}}
         animationType='fade'
-        backdropPressToClose={true}
         cancelContainerStyle={{display: 'none'}}
         selectStyle={{backgroundColor: 'white', borderColor: 'black', paddingLeft: 12, paddingRight: 12 }}
-        /* selectedItemTextStyle={{color: 'black'}} */
       />
     </MainContainer>
   )
 }
 
-export default SitePicker
+export default SitePickerSearchable
