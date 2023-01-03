2958dd7af13be595658ec3308265e1ecfed086e5
micpob <bomboloapp@gmail.com>
2023-01-03 12:40:53 +0100

    Add new sites

--- src/utils/defaultSites.js
+++ src/utils/defaultSites.js
@@ -1,15 +1,25 @@
 
 const sites = [
-  {value: 'askubuntu', name: 'Ask Ubuntu'},
-  {value: 'english', name: 'English Language & Usage'},
-  {value: 'italian', name: 'Italian Language'},
-  {value: 'math', name: 'Mathematics'},
-  {value: 'scifi', name: 'Science Fiction & Fantasy'},
-  {value: 'cooking', name: 'Seasoned Advice'},
-  {value: 'serverfault', name: 'Server Fault'},
-  {value: 'stackoverflow', name: 'Stack Overflow'},
-  {value: 'superuser', name: 'Superuser'},
-  {value: 'travel', name: 'Travel'},
+  { value: 'gaming', name: 'Arqade' },
+  { value: 'askubuntu', name: 'Ask Ubuntu' },
+  { value: 'bicycles', name: 'Bicycles' },
+  { value: 'dba', name: 'Database Administrators' },
+  { value: 'english', name: 'English Language & Usage' },
+  { value: 'homebrew', name: 'Homebrewing' },
+  { value: 'italian', name: 'Italian Language' },
+  { value: 'math', name: 'Mathematics' },
+  { value: 'photo', name: 'Photography' },
+  { value: 'salesforce', name: 'Salesforce' },
+  { value: 'scifi', name: 'Science Fiction & Fantasy' },
+  { value: 'cooking', name: 'Seasoned Advice' },
+  { value: 'serverfault', name: 'Server Fault' },
+  { value: 'stackoverflow', name: 'Stack Overflow' },
+  { value: 'superuser', name: 'Superuser' },
+  { value: 'cstheory', name: 'Theoretical Computer Science' },
+  { value: 'travel', name: 'Travel' },
+  { value: 'unix', name: 'Unix & Linux' },
+  { value: 'ux', name: 'User Experience' },
+  { value: 'worldbuilding', name: 'Worldbuilding' }
 ]
 
 export default {sites}
